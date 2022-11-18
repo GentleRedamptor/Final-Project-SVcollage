6949f57c9c0bf0dbd3edf1f412a2111769d0ada7
GentleRedamptor <noamfex13@gmail.com>
2022-11-18 19:50:26 +0200

    Finished the basic character controller
    Added looking moving and jumping, sprinting is needed as well, will add in a later commit.
    Also didn't bug check the jumping, maybe it will be needed to be fixed later.

--- Assets/Scripts/PlayerAndCamera.cs
+++ Assets/Scripts/PlayerAndCamera.cs
@@ -8,26 +8,70 @@ public class PlayerAndCamera : MonoBehaviour
     float mouseY;
     [SerializeField] float sensitivity;
     Transform rotateCam;
+    [SerializeField] float speed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpHeight = 3f;
+    CharacterController controller;
+    Vector3 velocity;
+    Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.4f;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
     void Start()
     {
-        sensitivity = 10;
-        rotateCam = 
-        
+        LockMouse();
+        //Getting Objects and Components
+        rotateCam = GameObject.Find("RotateCamJoint").GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+        groundCheck = GameObject.Find("GroundCheck").GetComponent<Transform>();        
     }
 
     void Update()
     {
+        CheckIfGrounded();
         CamControl();
-        
+        if (Input.GetButtonDown("Jump") && isGrounded) Jump();
+        PlayerMovement();
+        if (Input.GetKeyDown(KeyCode.Escape)) ReleaseMouse();
+                
     }
 
     void CamControl()
     {
-        mouseX = Input.GetAxis("Mouse X") * sensitivity;         
-        transform.localRotation = Quaternion.Euler(0f, mouseX, 0f);
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;         
+        transform.Rotate(Vector3.up * mouseX);
 
-        mouseY += Input.GetAxis("Mouse Y") * sensitivity;
+        mouseY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         mouseY = Mathf.Clamp(mouseY, -89.9f, 89.9f);
-        transform.localRotation = Quaternion.Euler(mouseY, 0f, 0f);
+        rotateCam.localRotation = Quaternion.Euler(-mouseY, 0f, 0f);
+    }
+    void PlayerMovement()
+    {
+        float horizontalinput = Input.GetAxis("Horizontal");
+        float verticalinput = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * horizontalinput + transform.forward * verticalinput;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(move * speed * Time.deltaTime); //move the character on the X and Z axis
+        controller.Move(velocity * Time.deltaTime); //apply gravity
+    }
+    void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+        if (isGrounded && velocity.y < 0) velocity.y = -2f; //Reseting fall force if grounded
+    }
+    void Jump()
+    {
+      velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+    void LockMouse()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void ReleaseMouse()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
     }
 }
