f354205a1c9fbeef4dc03b476c1396bf8741ae37
ElricSai <shonskarp@gmail.com>
2022-12-01 14:22:50 +0200

    health system(code only) and fixed the raycast hit
    player has 3 health, 1 health drops when enemy hits, enemy raycast finally hits player collider but coroutine still doesnt work well

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -10,11 +10,22 @@ public class EnemyBehaviour : MonoBehaviour
     // ready to shoot
     [SerializeField] float timeTillShoot;
     [SerializeField] bool CanSeePlayer;
-    [SerializeField] GameObject G;
-    
+    [SerializeField] GameObject EnemyBody;
+    [SerializeField] GameObject Target;
+    [SerializeField] GameObject PlayerTarget;
+    [SerializeField] GameObject saibaman;
+    [SerializeField] bool alreadyAttacked;
+    [SerializeField] LayerMask whatIsPlayer;
+    [SerializeField] float range;
+    [SerializeField] int dmg;
+
     // Start is called before the first frame update
-   private void Awake()
+    private void Awake()
     {
+        timeTillShoot = 5;
+        range = 100;
+        dmg = 1;
+        playerPos = GameObject.Find("Player").transform;
         CanSeePlayer = false;
        
     }
@@ -22,32 +33,45 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, playerPos.position) <= 10)
-        { 
-            CanSeePlayer = true;
-        TimeToShoot(); }
-        else
-        {
-            CanSeePlayer = false;
+        CanSeePlayer = Physics.CheckSphere(transform.position, range, whatIsPlayer);
+        if (CanSeePlayer)
+        { saibaman.transform.parent = PlayerTarget.transform;
 
+            TimeToShoot();
         }
-
-
     }
 
 
     private void TimeToShoot()
     {
-        if (CanSeePlayer)
+      transform.LookAt(playerPos);
+      StartCoroutine(Coloroutine(new Color[3] { Color.green, Color.yellow, Color.red }, 1, EnemyBody));
+        // int timeTillShoot = 5;
+        if (!alreadyAttacked )
         {
-            transform.LookAt(playerPos);
-            StartCoroutine(Coloroutine(new Color[3] { Color.green, Color.yellow, Color.red }, 1, G));
-            Destroy(GameObject.Find("Player"), 5);
+            RaycastHit hit;
+            if (Physics.Raycast(Target.transform.position, transform.forward, out hit, range))// range = 20
+            {
+                
+                Debug.Log(hit.transform.name);
+                if (hit.collider.name == "Player")
+               {
+                    PlayerAndCamera player = hit.transform.GetComponent<PlayerAndCamera>();
 
+                    player.takeDamage(dmg);
+               }
+            }
+            StopCoroutine(Coloroutine(new Color[3] { Color.green, Color.yellow, Color.red }, 1, EnemyBody));
+            alreadyAttacked = true;   
+            Invoke(nameof(ResetAttack), 3);
+           
         }
-       
+    }
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
 
-}
+    }
 
     IEnumerator Coloroutine(Color[] colors, float Time, GameObject GO)
     {
